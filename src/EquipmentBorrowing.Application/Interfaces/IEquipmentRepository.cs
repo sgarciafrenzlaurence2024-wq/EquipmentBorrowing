bc060f0808fdd84b7e162323d6b2fc9fa50b27c9
Frenz Laurence A. Garcia <s.garcia.frenzlaurence_2024@cmu.edu.ph>
2026-08-26 18:39:49 +0800

    Add repository interfaces in application layer

---
+++ src/EquipmentBorrowing.Application/Interfaces/IEquipmentRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquipmentBorrowing.Application.Interfaces
+{
+    internal class IEquipmentRepository
+    {
+    }
+}
---
+++ src/EquipmentBorrowing.Application/Interfaces/IStudentRepository.cs
@@ -0,0 +1,9 @@
+﻿using EquipmentBorrowing.Domain;
+
+namespace EquipmentBorrowing.Application.Interfaces;
+
+public interface IBorrowingRepository
+{
+    Task<int> GetActiveCountByStudentIdAsync(int studentId, CancellationToken ct = default);
+    Task AddAsync(Borrowing borrowing, CancellationToken ct = default);
+}
