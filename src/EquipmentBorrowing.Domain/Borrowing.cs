c741eee1233a894cdc2374f67b18edc7da4301bc
Frenz Laurence A. Garcia <s.garcia.frenzlaurence_2024@cmu.edu.ph>
2026-08-26 17:08:23 +0800

    Add domain models

---
+++ src/EquipmentBorrowing.Domain/Borrowing.cs
@@ -0,0 +1,21 @@
+﻿namespace EquipmentBorrowing.Domain;
+
+public class Borrowing
+{
+    public int Id { get; }
+    public int StudentId { get; }
+    public int EquipmentId { get; }
+    public DateTime DateBorrowed { get; }
+    public DateTime ExpectedReturnDate { get; }
+    public BorrowingStatus Status { get; private set; }
+
+    public Borrowing(int id, int studentId, int equipmentId, DateTime dateBorrowed, DateTime expectedReturnDate)
+    {
+        Id = id;
+        StudentId = studentId;
+        EquipmentId = equipmentId;
+        DateBorrowed = dateBorrowed;
+        ExpectedReturnDate = expectedReturnDate;
+        Status = BorrowingStatus.Active;
+    }
+}
---
+++ src/EquipmentBorrowing.Domain/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace EquipmentBorrowing.Domain;
+public class Student
+{
+    public int Id { get; }
+    public string Name { get; }
+    public bool IsAllowedToBorrow { get; set; }
+
+    public Student(int id, string name, bool isAllowedToBorrow = true)
+    {
+        Id = id;
+        Name = name;
+        IsAllowedToBorrow = isAllowedToBorrow;
+    }
+}
